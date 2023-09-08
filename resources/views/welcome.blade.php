d722d1344a09c9ee103757b8e15ec732fb4548a7
circlejourney <sceptile_rayquaza@hotmail.com>
2023-09-09 01:00:49 +1000

    Tidy landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 @extends("layouts.projects", ["projects" => $projects, "alternate" => false])
 
 @section("top")
-    <h2 class="center" style="color: #eee;">
+    <p class="center" style="color: #eee;">
         <b>Amari Low • they/them • Artist, writer, web developer, music producer... [<a href="/resume">view CV</a>]</b>
-    </h2>
+    </p>
     <div class="center">BFA Digital Animation • MFA Interactive Media • PhD candidate at QUT</div>
     <div class="spacer"></div>
     <div class="spacer"></div>
