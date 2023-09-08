6bd6e8478cda8a52160d90dfbeeeaa57977b3d63
circlejourney <sceptile_rayquaza@hotmail.com>
2023-09-09 01:06:06 +1000

    CSS cache busting and migration cleanup

--- resources/views/layouts/canonical.blade.php
+++ resources/views/layouts/canonical.blade.php
@@ -3,7 +3,7 @@
 @section("html_title") @yield("title") @endsection
 
 @push("head")
-    <link rel="stylesheet" href="/css/app.css?v=2">
+    <link rel="stylesheet" href="/css/app.css?v=3">
     <!--@ vite('resources/css/app.css')-->
     @yield("head")
 @endpush
