171fa6170d1d2aef28f254bac36d31f6b38aa576
circlejourney <sceptile_rayquaza@hotmail.com>
2023-09-09 18:04:47 +1000

    Update HTML banlist
    Updates the banlist to include elements that execute code on user action. WIP profanity list censoring, to be continued.

--- app/Http/Requests/TextUpdateRequest.php
+++ app/Http/Requests/TextUpdateRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\File;
 
 class TextUpdateRequest extends FormRequest
 {
@@ -14,14 +15,40 @@ class TextUpdateRequest extends FormRequest
      */
     public function rules(): array
     {
-        $this->request->set(
-            "body",
-            preg_replace(["/<\/?(a\s|script|style|link|meta|title)[^>]*>/"], "", $this->request->get("body")
-            )
-        );
+        $body = $this->transform($this->request->get("body"));
+        $this->request->set("body", $body);
         return [
             'title' => ['string', 'max:255'],
             'body' => ['string']
         ];
+    }
+
+    protected function transform($string) {
+        $profanity = json_decode(
+            File::get(base_path('resources'.DIRECTORY_SEPARATOR.'profanity.json')),
+            true
+        );
+        foreach($profanity as $term) {
+            $exceptions = $term["exceptions"] ?? false;
+            
+            if($exceptions) {
+                $exception_strings = array_map(function($i)use($term){
+                    return str_replace("*", $term["match"], $i);
+                }, $exceptions);
+                $string = str_replace(
+                    $exception_strings, $exceptions, $string
+                );
+            }
+
+            $string = preg_replace("/".$term["match"]."/", "[CENSORED]", $string);
+            $string = str_replace("*", $term["match"], $string);
+
+        }
+
+        $string = preg_replace(
+            ["/<\/?(script|style|link|meta|title|button|form|input)[^>]*>/", "/(<\/?.*)\son[A-Za-z]+\=\".*\"([^>]*>)/"],
+            ["", "$1$2"],
+            $string);
+        return $string;
     }
 }
