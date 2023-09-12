771edadfee46096e45914354739a69ed87dc899b
circlejourney <sceptile_rayquaza@hotmail.com>
2023-09-13 00:59:46 +1000

    Readability styling for music listings

--- resources/views/components/thumblist-2columns.blade.php
+++ resources/views/components/thumblist-2columns.blade.php
@@ -1,10 +1,10 @@
 <div class="bannergrid">
     @foreach($metalinks as $link)
     <x-thumb-banner class="bannerbutton-50" imgSrc="{{ $link->img_src }}">
-        <p>
-            <a href="{{ $link->href }}">{{ $link->title }}</a>
+        <h2><a href="{{ $link->href }}">{{ $link->title }}</a></h2>
+        <div>
             {!! $link->description !!}
-        </p>
+        </div>
         <span class="aside">
             {{ $link->publish_date_pretty() }}
         </span>
