4a89d2acfa597d4cd22de694b9879bde89997db3
circlejourney <sceptile_rayquaza@hotmail.com>
2026-06-27 16:02:40 +1000

    feat: Sync styling with root domain

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
 @extends("layouts.projects", ["projects" => $projects, "alternate" => false])
 
 @section("top")
-    <p class="center" style="color: #eee;">
-        <b>they/them • Artist, writer, web developer, music producer</b>
-    </p>
+    
+    <div id="tagline" style="text-align: center">
+        <b>🇸🇬 in 🇦🇺 • they/them • Artist, writer, web developer</b>
+        <br>
+        BFA (Hons.) Digital Animation • MFA Interactive Media • PhD Interaction Design
+    </div>
+    <div class="spacer"></div>
+    <div id="announcements" style="width: 100%; text-align: center">
+            I'm a storyteller and web developer whose heart still lives in the port I grew up beside. I create visual, written and interactive works about urban space, the sea, death and hauntings, mapping, and the many meanings of "home" and "place".
+    </div>
+
     <div class="spacer"></div>
     <div class="spacer"></div>
     <div class="center announcements">
